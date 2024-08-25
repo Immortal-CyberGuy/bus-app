0fa3b4f2ff9f01aaebd043063d4c9e3834fd3aac
Shubham Garg <142537083+Immortal-CyberGuy@users.noreply.github.com>
2024-08-25 19:17:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import Home from "./pages/Home"
-import About from './pages/About'
-import Contact from './pages/Contact'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Signup from "./pages/Signup"
-import Login from "./pages/login"
+import Home from "./pages/Home";
+import About from './pages/About';
+import Contact from './pages/Contact';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/login";
+import Footer from "./components/Footer"; // new footer component
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path:'/',
-      element:<><Navbar/><Home/></>
+      element:<><Navbar/><Home/><Footer/></> // added footer to home page
     },
     {
       path:'/about',
-      element:<><Navbar/><About/></>
+      element:<><Navbar/><About/><Footer/></> // added footer to about page
     },
     {
       path:'/contact',
-      element:<><Navbar/><Contact/></>
+      element:<><Navbar/><Contact/><Footer/></> // added footer to contact page
     },
     {
       path:'/signup',
-      element:<><Signup/></>
+      element:<><Signup/><Footer/></> // added footer to signup page
     },
     {
       path:'/login',
-      element:<><Login/></>
+      element:<><Login/><Footer/></> // added footer to login page
     }
   ])
   return (
